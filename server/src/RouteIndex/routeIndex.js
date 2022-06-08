fccc5bddec415f504e5a65207ac20de1b0c92650
JustBecerra <justj.becerra@gmail.com>
2022-06-07 22:00:38 -0300

    reset backend

--- server/src/RouteIndex/routeIndex.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// const RouterLocations = require('../Routes/locations')
-// const RouterProperties = require('../Routes/properties')
-// router.use('/locations', RouterLocations)
-// router.use('/properties', RouterProperties)
-
-module.exports = router;
