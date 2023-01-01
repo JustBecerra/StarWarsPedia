a98ed8b2bb852a640155fc4381ca043e88c7547c
Justo Becerra <justj.becerra@gmail.com>
2023-01-01 20:00:51 -0300

    added details to complete Info and changed home page

--- client/src/components/molecules/OpeningInfo.tsx
+++ client/src/components/molecules/OpeningInfo.tsx
@@ -1,14 +1,57 @@
 import { Container } from "@mui/system";
-import WelcomeText from "../atoms/WelcomeText";
-import WelcomeInfo from "../atoms/WelcomeInfo";
-import RepublicLogo from "../atoms/RepublicLogo";
+import { Box, Typography } from "@mui/material";
+import jedilogo from "../../imgs/jedilogo.png";
+import { styled } from "@mui/material/styles";
 
-const OpeningInfo = () => (
-  <Container>
-    <WelcomeText />
-    <RepublicLogo />
-    <WelcomeInfo />
-  </Container>
-);
+const OpeningInfo = () => {
+  const IntroLogo = styled("img")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    width: 300,
+  }));
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        width: 11 / 12,
+        height: 600,
+        backgroundColor: "#13191a",
+        justifySelf: "center",
+        marginTop: 12,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          width: 600,
+          flexDirection: "column",
+        }}
+      >
+        <Typography variant="h1" color="#be9c65" pt={8}>
+          Hello there!
+        </Typography>
+        <Typography variant="h5" color="#be9c65" pt={6}>
+          A long time ago in a galaxy far, far away....
+        </Typography>
+        <Typography variant="body1" color="#ffffff" pt={2}>
+          A young Software Developer decided to create this site so people can
+          read about specific topics after watching a Star Wars movie or TV
+          show. Currently it displays most of the canon information from the
+          original and prequel trilogies but hopefully users will add more
+          information as time goes on.
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          width: 300,
+          marginLeft: 10,
+        }}
+      >
+        <IntroLogo src={jedilogo} />
+      </Box>
+    </Container>
+  );
+};
 
 export default OpeningInfo;
--- client/src/components/templates/MainCard.tsx
+++ client/src/components/templates/MainCard.tsx
@@ -1,21 +1,9 @@
-import { Container, Box } from "@mui/material";
+import { Container } from "@mui/material";
 import OpeningInfo from "../molecules/OpeningInfo";
 
 const MainCard = () => (
   <Container>
-    <Box
-      sx={{
-        width: 1000,
-        height: 600,
-        backgroundColor: "#13191a",
-        marginRight: "auto",
-        marginLeft: "auto",
-        marginTop: 11,
-        // paddingTop: '100px',
-      }}
-    >
-      <OpeningInfo />
-    </Box>
+    <OpeningInfo />
   </Container>
 );
 
